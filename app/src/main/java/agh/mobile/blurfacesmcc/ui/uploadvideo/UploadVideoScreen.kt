038ef73b23cef53acc5099f33806dc652701758f
youngdashu <anowak2000@gmail.com>
2023-12-07 10:22:22 +0100

    enhanced uploadVideoScreen

--- app/src/main/java/agh/mobile/blurfacesmcc/ui/uploadvideo/UploadVideoScreen.kt
+++ app/src/main/java/agh/mobile/blurfacesmcc/ui/uploadvideo/UploadVideoScreen.kt
@@ -1,18 +1,26 @@
 package agh.mobile.blurfacesmcc.ui.uploadvideo
 
+import agh.mobile.blurfacesmcc.R
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,7 +28,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -41,39 +51,72 @@ fun UploadVideoScreen(
     }
 
 
+    LaunchedEffect(null) {
+        launcher.launch(
+            PickVisualMediaRequest(mediaType = ActivityResultContracts.PickVisualMedia.ImageAndVideo)
+        )
+    }
+
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
+
+    var videoTitle by remember {
+        mutableStateOf("")
+    }
+
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Spacer(modifier = Modifier.height(40.dp))
 
-        Button(
-            onClick = {
-                launcher.launch(
-                    PickVisualMediaRequest(mediaType = ActivityResultContracts.PickVisualMedia.ImageAndVideo)
-                )
-            }
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = "Choose a media")
+            Button(
+                onClick = {
+                    launcher.launch(
+                        PickVisualMediaRequest(mediaType = ActivityResultContracts.PickVisualMedia.ImageAndVideo)
+                    )
+                }
+            ) { Text(text = "Choose a media") }
         }
+    }
 
-        result?.let { image ->
-            //Use Coil to display the selected image
-            val painter = rememberAsyncImagePainter(
-                ImageRequest
-                    .Builder(LocalContext.current)
-                    .data(data = image)
-                    .build()
-            )
-            Image(
-                painter = painter,
-                contentDescription = null,
-                modifier = Modifier
-                    .size(400.dp, 400.dp)
-                    .padding(16.dp)
-            )
+    val context = LocalContext.current
+
+    if (showDialog) {
+        Dialog(onDismissRequest = { showDialog = false }) {
+            Card {
+                result?.let { image ->
+                    //Use Coil to display the selected image
+                    val painter = rememberAsyncImagePainter(
+                        ImageRequest
+                            .Builder(LocalContext.current)
+                            .data(data = image)
+                            .build()
+                    )
+                    Image(
+                        painter = painter,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(400.dp, 400.dp)
+                            .padding(16.dp)
+                    )
+                }
+                OutlinedTextField(
+                    value = videoTitle,
+                    onValueChange = { videoTitle = it }
+                )
+                TextButton(onClick = {
+                    uploadVideoViewModel.uploadVideoForProcessing(context, result!!)
+                }) {
+                    Text(text = stringResource(id = R.string.blur_faces))
+                }
+            }
         }
-
-
     }
 }
--- app/src/main/java/agh/mobile/blurfacesmcc/ui/uploadvideo/UploadVideoViewModel.kt
+++ app/src/main/java/agh/mobile/blurfacesmcc/ui/uploadvideo/UploadVideoViewModel.kt
@@ -1,9 +1,17 @@
 package agh.mobile.blurfacesmcc.ui.uploadvideo
 
+import android.content.Context
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class UploadVideoViewModel @Inject constructor(
-) : ViewModel()
+) : ViewModel() {
+
+    fun uploadVideoForProcessing(context: Context, uri: Uri) {
+
+    }
+
+}
