016b05207aeff85409557ea346279511262608e4
Krzysztof Gądek <krzysztof.gadek15@gmail.com>
2023-12-12 19:04:09 +0100

    Added scaffold with floating button

--- app/src/main/java/agh/mobile/blurfacesmcc/ui/securitysettings/confidentialData/ConfidentialDataScreen.kt
+++ app/src/main/java/agh/mobile/blurfacesmcc/ui/securitysettings/confidentialData/ConfidentialDataScreen.kt
@@ -1,10 +1,28 @@
 package agh.mobile.blurfacesmcc.ui.securitysettings.confidentialData
 
+import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
-fun ConfidentialDataScreen(
-
-) {
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+fun ConfidentialDataScreen() {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = MaterialTheme.colorScheme.primary,
+                onClick = { /*TODO*/ },
+            ) {
+                Icon(Icons.Filled.Add, "Floating action button.")
+            }
+        }
+    ) { contentPadding ->
+        // TODO
+    }
 
 }
