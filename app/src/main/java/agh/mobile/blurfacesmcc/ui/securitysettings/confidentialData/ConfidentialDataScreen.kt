ecad881bf4874802d3e3bbd21bb1dac05c13f0e0
Krzysztof Gądek <krzysztof.gadek15@gmail.com>
2024-01-03 23:15:37 +0100

    can view added image

--- app/src/main/java/agh/mobile/blurfacesmcc/ui/securitysettings/confidentialData/ConfidentialDataScreen.kt
+++ app/src/main/java/agh/mobile/blurfacesmcc/ui/securitysettings/confidentialData/ConfidentialDataScreen.kt
@@ -1,10 +1,14 @@
 package agh.mobile.blurfacesmcc.ui.securitysettings.confidentialData
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -18,7 +22,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -26,6 +36,7 @@ fun ConfidentialDataScreen(
     viewModel: ConfidentialDataViewModel = hiltViewModel()
 ) {
     val confidentialData by viewModel.confidentialData.collectAsState()
+    val context = LocalContext.current
 
     var result by remember {
         mutableStateOf<Uri?>(null)
@@ -53,7 +64,21 @@ fun ConfidentialDataScreen(
     ) { contentPadding ->
         confidentialData?.let {
             it.map {
-                Text(text = it.uri)
+                context.contentResolver.takePersistableUriPermission(
+                    Uri.parse(it.uri),
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                )
+                val model = ImageRequest.Builder(context)
+                    .data(Uri.parse(it.uri))
+                    .build()
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    AsyncImage(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(200.dp), model = model, contentDescription = ""
+                    )
+                    Text(text = it.filename)
+                }
             }
         }
     }
