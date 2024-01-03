b9b87df7b72a850e6a5846aa848a4d633f7ad29c
Krzysztof Gądek <krzysztof.gadek15@gmail.com>
2024-01-03 22:52:43 +0100

    can add confidential data

--- app/src/main/java/agh/mobile/blurfacesmcc/ui/securitysettings/confidentialData/ConfidentialDataScreen.kt
+++ app/src/main/java/agh/mobile/blurfacesmcc/ui/securitysettings/confidentialData/ConfidentialDataScreen.kt
@@ -3,6 +3,7 @@ package agh.mobile.blurfacesmcc.ui.securitysettings.confidentialData
 import android.annotation.SuppressLint
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -10,15 +11,22 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-fun ConfidentialDataScreen() {
+fun ConfidentialDataScreen(
+    viewModel: ConfidentialDataViewModel = hiltViewModel()
+) {
+    val confidentialData by viewModel.confidentialData.collectAsState()
+
     var result by remember {
         mutableStateOf<Uri?>(null)
     }
@@ -26,20 +34,28 @@ fun ConfidentialDataScreen() {
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickVisualMedia()
     ) {
-        result = it
+        it?.let { viewModel.saveUri(it) }
     }
 
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
                 containerColor = MaterialTheme.colorScheme.primary,
-                onClick = { /*TODO*/ },
+                onClick = {
+                    launcher.launch(
+                        PickVisualMediaRequest(mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly)
+                    )
+                },
             ) {
                 Icon(Icons.Filled.Add, "Floating action button.")
             }
         }
     ) { contentPadding ->
-        // TODO
+        confidentialData?.let {
+            it.map {
+                Text(text = it.uri)
+            }
+        }
     }
 
 }
