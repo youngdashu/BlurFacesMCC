d27dbfaf46fcec370c33d12efa509020a298a831
Krzysztof Gądek <krzysztof.gadek15@gmail.com>
2024-01-03 23:38:53 +0100

    ui enhancements

--- app/src/main/java/agh/mobile/blurfacesmcc/ui/securitysettings/confidentialData/ConfidentialDataScreen.kt
+++ app/src/main/java/agh/mobile/blurfacesmcc/ui/securitysettings/confidentialData/ConfidentialDataScreen.kt
@@ -6,9 +6,15 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -19,11 +25,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -37,10 +42,6 @@ fun ConfidentialDataScreen(
 ) {
     val confidentialData by viewModel.confidentialData.collectAsState()
     val context = LocalContext.current
-
-    var result by remember {
-        mutableStateOf<Uri?>(null)
-    }
 
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickVisualMedia()
@@ -62,25 +63,49 @@ fun ConfidentialDataScreen(
             }
         }
     ) { contentPadding ->
-        confidentialData?.let {
-            it.map {
-                context.contentResolver.takePersistableUriPermission(
-                    Uri.parse(it.uri),
-                    Intent.FLAG_GRANT_READ_URI_PERMISSION
-                )
-                val model = ImageRequest.Builder(context)
-                    .data(Uri.parse(it.uri))
-                    .build()
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    AsyncImage(
-                        modifier = Modifier
-                            .width(200.dp)
-                            .height(200.dp), model = model, contentDescription = ""
+        val scrollState = rememberScrollState()
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(scrollState)) {
+            confidentialData?.let {
+                it.map {
+                    context.contentResolver.takePersistableUriPermission(
+                        Uri.parse(it.uri),
+                        Intent.FLAG_GRANT_READ_URI_PERMISSION
                     )
-                    Text(text = it.filename)
+                    val model = ImageRequest.Builder(context)
+                        .data(Uri.parse(it.uri))
+                        .build()
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 15.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .padding(5.dp)
+                                .clip(RoundedCornerShape(15.dp, 15.dp, 15.dp, 15.dp))
+                                .background(Color.LightGray),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            AsyncImage(
+                                modifier = Modifier.fillMaxWidth(0.7f),
+                                model = model,
+                                contentDescription = ""
+                            )
+                            Text(text = it.filename)
+                        }
+
+                    }
                 }
             }
+
+
         }
+
     }
 
 }
