8927801ddc9757e4c74d984601572b1b03e5ddd5
Krzysztof Gądek <44200187+ChrisGadek1@users.noreply.github.com>
2024-01-18 11:55:20 +0100

    pinging backend for results (#21)

--- app/src/main/java/agh/mobile/blurfacesmcc/workers/RemoteBlurWorker.kt
+++ app/src/main/java/agh/mobile/blurfacesmcc/workers/RemoteBlurWorker.kt
@@ -6,6 +6,7 @@ import agh.mobile.blurfacesmcc.ui.util.process.updateVideoInDataStore
 import android.content.Context
 import android.net.Uri
 import android.os.Environment
+import android.util.Log
 import androidx.core.net.toUri
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
@@ -13,9 +14,11 @@ import androidx.work.WorkerParameters
 import androidx.work.workDataOf
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.delay
 import java.io.File
 import java.io.FileOutputStream
 import java.net.URL
+import kotlin.time.TimeSource
 
 
 @HiltWorker
@@ -26,6 +29,9 @@ class RemoteBlurWorker @AssistedInject constructor(
 ) : CoroutineWorker(context, parameters) {
 
     override suspend fun doWork(): Result {
+        val timeSource = TimeSource.Monotonic
+        val startTime = timeSource.markNow()
+
         val rawUri = inputData.getString(URI_KEY)
         val videoTitle = inputData.getString(VIDEO_TITLE_KEY)!!
 
@@ -36,20 +42,30 @@ class RemoteBlurWorker @AssistedInject constructor(
             val file = inputStream.readAllBytes()
             inputStream.close()
             val url = videosRepository.processRemote(UploadVideoRequest(file, videoTitle))
-            val data = URL(url).readBytes()
-            val outputDir =
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES)
-                    .toString()
-            val outputFile = File("$outputDir/$videoTitle.mp4")
-            val stream = FileOutputStream(outputFile)
-            stream.write(data)
-            stream.close()
-            context.updateVideoInDataStore(Uri.parse(rawUri)) {
-                uri = outputFile.toUri().toString()
-                blured = true
+            (0..5000).forEach { _ ->
+                try {
+                    val data = URL(url).readBytes()
+                    val outputDir =
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES)
+                            .toString()
+                    val outputFile = File("$outputDir/$videoTitle.mp4")
+                    val stream = FileOutputStream(outputFile)
+                    stream.write(data)
+                    stream.close()
+                    context.updateVideoInDataStore(Uri.parse(rawUri)) {
+                        uri = outputFile.toUri().toString()
+                        blured = true
+                    }
+                    val endTime = timeSource.markNow() - startTime
+                    Log.d("perf", endTime.toString())
+                    return Result.success()
+                } catch (e: Exception) {
+                    delay(5000)
+                }
             }
-
-            return Result.success()
+            return Result.failure(
+                workDataOf("error" to "Timeout")
+            )
         } catch (e: Exception) {
             context.updateVideoInDataStore(Uri.parse(rawUri)) {
                 filename = "FAILED_$videoTitle"
